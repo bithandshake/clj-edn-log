52fb6902b0a7d2942c4118ed2752eab0a0c0d909
Bit Handshake <adam@monotech.hu>
2022-11-23 20:05:52 +0100

    0.1.1.4

--- source-code/clj/logger/side_effects.clj
+++ source-code/clj/logger/side_effects.clj
@@ -17,4 +17,4 @@
   (let [filepath  (str config/LOG-PATH "/" filename)
         timestamp (time/timestamp-string)
         output    (str timestamp " " content)]
-       (io/prepend-to-file! filepath output {:max-line-count config/MAX-LINE-COUNT})))
+       (io/prepend-to-file! filepath output {:create? true :max-line-count config/MAX-LINE-COUNT})))
